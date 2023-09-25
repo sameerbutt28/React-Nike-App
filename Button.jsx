8f4ab3324459450dfeb781858053a6842f509950
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-09-25 15:19:57 +0500

    Add files via upload

--- Button.jsx
+++ Button.jsx
@@ -1,7 +1,8 @@
 
+// import PropTypes from 'prop-types';
 const Button = ({label, iconURL}) => {
   return (
-<button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+<button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg bg-coral-red  leading-none  rounded-full text-white border-coral-red   ">
 {label}
 <img src={iconURL} alt="arrow right icon"
 className="ml-2 rounded-full w-5 h-5"/>
