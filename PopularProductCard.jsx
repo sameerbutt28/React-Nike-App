5eb768846f64cf10dd1283916f12e794099a5ff3
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-09-26 00:55:13 +0500

    Add files via upload

--- PopularProductCard.jsx
+++ PopularProductCard.jsx
@@ -1,4 +1,4 @@
-// import star from '../assets/assets/icons'
+// import {star} from '../assets/assets/images'
 const PopularProductCard = ({imgURL, name, price }) => {
   return (
     <div className='flex flex-1 flex-col w-full max-sm:w-full'>
